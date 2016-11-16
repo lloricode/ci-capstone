2c40b6045c76b0c6d8582c0de6569700c8f3d1fa
Lloric <emorickfighter@gmail.com>
2016-11-16 13:56:34 +0800

    testing for creating defind class

---
+++ application/libraries/Admin.php
@@ -0,0 +1,16 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class Admin {
+
+    private $id;
+    private $fullname;
+    private $username;
+    private $password;
+    private $status;
+    
+    
+
+}
