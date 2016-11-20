d710da7916180603ed8dbd4d61ee5f602bf3b772
lloric <lloric@lloric>
2016-11-20 15:09:29 +0800

    home_top view

---
+++ application/views/admin/home_top.php
@@ -0,0 +1,55 @@
+<?php defined('BASEPATH') or exit('not allowed!'); ?>
+<div class="row">
+    <div class="col-xs-12 col-md-6 col-lg-3">
+        <div class="panel panel-teal panel-widget">
+            <div class="row no-padding">
+                <div class="col-sm-3 col-lg-5 widget-left">
+                    <svg class="glyph stroked male-user"><use xlink:href="#stroked-male-user"></use></svg>
+                </div>
+                <div class="col-sm-9 col-lg-7 widget-right">
+                    <div class="large">24</div>
+                    <div class="text-muted">Users</div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-xs-12 col-md-6 col-lg-3">
+        <div class="panel panel-blue panel-widget ">
+            <div class="row no-padding">
+                <div class="col-sm-3 col-lg-5 widget-left">
+                    <svg class="glyph stroked bag"><use xlink:href="#stroked-bag"></use></svg>
+                </div>
+                <div class="col-sm-9 col-lg-7 widget-right">
+                    <div class="large">120</div>
+                    <div class="text-muted">New Orders</div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-xs-12 col-md-6 col-lg-3">
+        <div class="panel panel-orange panel-widget">
+            <div class="row no-padding">
+                <div class="col-sm-3 col-lg-5 widget-left">
+                    <svg class="glyph stroked empty-message"><use xlink:href="#stroked-empty-message"></use></svg>
+                </div>
+                <div class="col-sm-9 col-lg-7 widget-right">
+                    <div class="large">52</div>
+                    <div class="text-muted">Comments</div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-xs-12 col-md-6 col-lg-3">
+        <div class="panel panel-red panel-widget">
+            <div class="row no-padding">
+                <div class="col-sm-3 col-lg-5 widget-left">
+                    <svg class="glyph stroked app-window-with-content"><use xlink:href="#stroked-app-window-with-content"></use></svg>
+                </div>
+                <div class="col-sm-9 col-lg-7 widget-right">
+                    <div class="large">25.2k</div>
+                    <div class="text-muted">Page Views</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div><!--/.row-->
