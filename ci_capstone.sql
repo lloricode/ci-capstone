6556e0429b0b0a43cee8c6f9395bc35eeef6a7f3
lloric <lloric@lloric>
2016-12-09 18:45:58 +0800

    database add url table

--- ci_capstone.sql
+++ ci_capstone.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.11
+-- version 4.5.2
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Nov 14, 2016 at 04:36 PM
--- Server version: 5.6.21
--- PHP Version: 5.6.3
+-- Host: localhost
+-- Generation Time: Dec 09, 2016 at 11:44 AM
+-- Server version: 10.1.19-MariaDB
+-- PHP Version: 5.6.24
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `ci_capstone`
@@ -26,13 +26,13 @@ SET time_zone = "+00:00";
 -- Table structure for table `admin`
 --
 
-CREATE TABLE IF NOT EXISTS `admin` (
-`admin_id` int(11) NOT NULL,
+CREATE TABLE `admin` (
+  `admin_id` int(11) NOT NULL,
   `admin_fullname` varchar(20) NOT NULL,
   `admin_username` varchar(20) NOT NULL,
   `admin_password` varchar(500) NOT NULL,
-  `admin_status` tinyint(1) NOT NULL DEFAULT '1'
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+  `admin_status` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `admin`
@@ -40,6 +40,21 @@ CREATE TABLE IF NOT EXISTS `admin` (
 
 INSERT INTO `admin` (`admin_id`, `admin_fullname`, `admin_username`, `admin_password`, `admin_status`) VALUES
 (1, 'Im Root', 'root', '$2y$10$BtJSAZYGTWeDMriJMNJzDuRvIRAMbdyXEq5jSA45Ape5hEF8p0exG', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `url`
+--
+
+CREATE TABLE `url` (
+  `url_id` int(11) NOT NULL,
+  `url_value` varchar(2000) NOT NULL,
+  `url_count` int(11) NOT NULL DEFAULT '1',
+  `url_agent` varchar(50) NOT NULL,
+  `url_platform` varchar(50) NOT NULL,
+  `admin_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
@@ -49,7 +64,13 @@ INSERT INTO `admin` (`admin_id`, `admin_fullname`, `admin_username`, `admin_pass
 -- Indexes for table `admin`
 --
 ALTER TABLE `admin`
- ADD PRIMARY KEY (`admin_id`);
+  ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indexes for table `url`
+--
+ALTER TABLE `url`
+  ADD PRIMARY KEY (`url_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -59,7 +80,12 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+--
+-- AUTO_INCREMENT for table `url`
+--
+ALTER TABLE `url`
+  MODIFY `url_id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
