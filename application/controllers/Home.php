51aaa3ee59f1cdc3a020f11ec8b687f8625549f6
lloric <lloric@lloric>
2016-12-10 01:49:17 +0800

    merge

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -9,3 +9,27 @@ class Home extends MY_Controller {
     }
 
 }
+// yo PRE.
+/*
+Suggestion lang, what if ganito ang controllers mo para mas simple?
+
+sample:
+controller
+ -> action A
+ -> action B
+
+ Suggestion:
+
+ HomeController (For info pages)
+ -> index
+ -> about
+ -> contact
+
+ AccountController (For account related pages)
+ ->login
+ ->logout
+ ->signup
+ ->index (account dashboard, pag hindi authenticated redirect to login action) 
+
+
+*/
--- readme.rst
+++ readme.rst
@@ -4,6 +4,7 @@ CI Capstone
 
 Thesis projects powered by Codeigniter 3.1.2 a php framework, also bootstrap framework
 
-
+PHP Framework : Codeigniter 3.1.2
+UI Framework  : bootstrap
 
 *******************
