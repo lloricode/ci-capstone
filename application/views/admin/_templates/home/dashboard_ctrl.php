ed1c53a3cf1ebb654dfbd81182bd520802d0d158
iEdzar <lightningzeay@yahoo.com>
2017-02-09 14:14:52 +0800

    Added Dashboard control

---
+++ application/views/admin/_templates/home/dashboard_ctrl.php
@@ -0,0 +1,14 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<div class="container-fluid">
+    <div class="quick-actions_homepage">
+        <ul class="quick-actions">
+
+            <li class="bg_ly"> <a href= "<?php echo base_url('admin/users'); ?>"> <i class="icon-pencil"></i> <span class="label label-important"></span>Enroll Student</a> </li>
+            <li class="bg_ls"> <a href="<?php echo base_url('admin/students'); ?>"> <i class="icon-user"></i> <span class="label label-important"></span>Add Teacher</a> </li>
+            <li class="bg_lg"> <a href="<?php echo base_url('admin/students'); ?>"> <i class="icon-cogs"></i> <span class="label label-important"></span>Hash Link</a> </li>
+            <li class="bg_lo"> <a href= "<?php echo base_url('admin/users'); ?>"> <i class="icon-download-alt"></i> <span class="label label-important"></span>Hash Link</a> </li>
+           
+
+        </ul>
+    </div>
+</div>
--- application/views/admin/_templates/home/user_count.php
+++ application/views/admin/_templates/home/user_count.php
@@ -13,7 +13,7 @@
             <li class="bg_lg"> <a href="calendar.html"> <i class="icon-calendar"></i> Calendar</a> </li>
             <li class="bg_lr"> <a href="error404.html"> <i class="icon-info-sign"></i> Error</a> </li>-->
             <li class="bg_lb"> <a href= "<?php echo base_url('admin/users'); ?>"> <i class="icon-group"></i> <span class="label label-important"><?php echo $active_users ?></span>Active Users </a> </li>
-            <li class="bg_lg"> <a href="<?php echo base_url('admin/students'); ?>"> <i class="icon-user"></i> <span class="label label-important"><?php echo $active_students ?></span> Active Students </a> </li>
+            <li class="bg_lg"> <a href="<?php echo base_url('admin/students'); ?>"> <i class="icon-user"></i> <span class="label label-important"><?php echo $active_students ?></span> Enrolled Students </a> </li>
 
         </ul>
     </div>
