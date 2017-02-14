1ead00ffb5c71767df12752134449e539a06b83c
shikai06 <pojinkee1@gmail.com>
2017-02-14 15:46:38 +0800

    Subject Offers adding and viewing not yet complete

--- application/controllers/Create_subject_offer.php
+++ application/controllers/Create_subject_offer.php
@@ -71,20 +71,22 @@ class Create_subject_offer extends CI_Capstone_Controller
                 $this->data['message'] = (validation_errors() ? validation_errors() : ($this->ion_auth->errors() ? $this->ion_auth->errors() : $this->session->flashdata('message')));
 
                 $this->data['subject_offer_start'] = array(
-                    'name'        => 'subject_offer_start',
-                    'time_format' => 'mm-dd-yyyy',
-                    'class'       => 'datepicker',
-                    'value'       => $this->form_validation->set_value('subject_offer_start'),
+                    'name'   => 'subject_offer_start',
+                    'format' => 'HH:mm',
+                    'type'   => 'time',
+                    'value'  => $this->form_validation->set_value('subject_offer_start'),
                 );
-                $this->data['subject_offer_end']   = array(
-                    'name'             => 'subject_offer_end',
-                    'data-date-format' => 'mm-dd-yyyy',
-                    'class'            => 'datepicker',
-                    'value'            => $this->form_validation->set_value('subject_offer_end'),
+
+
+                $this->data['subject_offer_end'] = array(
+                    'name'   => 'subject_offer_end',
+                    'format' => 'HH:mm',
+                    'type'   => 'time',
+                    'value'  => $this->form_validation->set_value('subject_offer_end'),
                 );
-                $this->data['user_id_value']       = $this->User_model->as_dropdown('username')->get_all();
-                $this->data['subject_id_value']    = $this->Subject_model->as_dropdown('subject_code')->get_all();
-                $this->data['room_id_value']       = $this->Room_model->as_dropdown('room_number')->get_all();
+                $this->data['user_id_value']     = $this->User_model->as_dropdown('username')->get_all();
+                $this->data['subject_id_value']  = $this->Subject_model->as_dropdown('subject_code')->get_all();
+                $this->data['room_id_value']     = $this->Room_model->as_dropdown('room_number')->get_all();
 
 
                 $this->data['bootstrap'] = $this->bootstrap();
