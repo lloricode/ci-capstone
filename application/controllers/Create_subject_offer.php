f84ebb5c0ee4d6f76fd2d4df75c08c3eb03a5043
shikai06 <pojinkee1@gmail.com>
2017-02-14 16:32:04 +0800

    changed something

--- application/controllers/Create_subject_offer.php
+++ application/controllers/Create_subject_offer.php
@@ -71,18 +71,16 @@ class Create_subject_offer extends CI_Capstone_Controller
                 $this->data['message'] = (validation_errors() ? validation_errors() : ($this->ion_auth->errors() ? $this->ion_auth->errors() : $this->session->flashdata('message')));
 
                 $this->data['subject_offer_start'] = array(
-                    'name'   => 'subject_offer_start',
-                    'format' => 'HH:mm',
-                    'type'   => 'time',
-                    'value'  => $this->form_validation->set_value('subject_offer_start'),
+                    'name'  => 'subject_offer_start',
+                    'type'  => 'time',
+                    'value' => $this->form_validation->set_value('subject_offer_start'),
                 );
 
 
                 $this->data['subject_offer_end'] = array(
-                    'name'   => 'subject_offer_end',
-                    'format' => 'HH:mm',
-                    'type'   => 'time',
-                    'value'  => $this->form_validation->set_value('subject_offer_end'),
+                    'name'  => 'subject_offer_end',
+                    'type'  => 'time',
+                    'value' => $this->form_validation->set_value('subject_offer_end'),
                 );
                 $this->data['user_id_value']     = $this->User_model->as_dropdown('username')->get_all();
                 $this->data['subject_id_value']  = $this->Subject_model->as_dropdown('subject_code')->get_all();
