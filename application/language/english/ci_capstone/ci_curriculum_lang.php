7997527572785577ad2d959f570879b27e227b73
Lloric Mayuga Garcia <lloricode@users.noreply.github.com>
2017-03-09 14:00:02 +0800

    Revert "spelling"

--- application/language/english/ci_capstone/ci_curriculum_lang.php
+++ application/language/english/ci_capstone/ci_curriculum_lang.php
@@ -1,25 +1,29 @@
 <?php
 
-/**
+/*
  * ENGLISH
  */
 defined('BASEPATH') or exit('Direct Script is not allowed');
 
-$lang['curriculum_label']        = 'Curriculum';
+$lang['curriculum_label']        = 'Curriculums';
 $lang['create_curriculum_label'] = 'Create Curriculum';
-$lang['view_curriculum_label']   = 'View Curriculums';
+$lang['view_curriculum_label']   = 'View Curriculum';
 
 
 
 $lang['curriculumn_description']        = 'Description';
-$lang['curriculumn_effective_year']     = 'Effective Year';
+$lang['curriculumn_effective_year']     = 'Academic Year';
 $lang['curriculumn_effective_semester'] = 'Effective Semester';
 $lang['curriculumn_course']             = 'Course Code';
 $lang['curriculumn_status']             = 'Status';
-
 //other
 
 $lang['curriculumn_create_button'] = 'Create Curriculum';
 $lang['curriculumn_status_enable'] = 'Enable';
 $lang['curriculumn_option']        = 'Option';
 $lang['curriculumn_view']          = 'View';
+
+
+
+
+
