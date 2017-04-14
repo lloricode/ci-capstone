5b14f5a59638d62550084939db00f5ef3fb5db09
lloricode <emorickfighter@gmail.com>
2017-04-14 20:17:52 +0800

    disable temporary pdf

--- application/libraries/Pdf.php
+++ application/libraries/Pdf.php
@@ -1,34 +1,34 @@
 <?php
-
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-require_once('assets/libraries/dompdf/autoload.inc.php');
-
-use Dompdf\Dompdf;
-
-class Pdf
-{
-
-
-        private $dompdf;
-
-        public function __construct()
-        {
-                $this->dompdf = new Dompdf();
-        }
-
-        public function print_now($data, $orientation = 'portrait')
-        {
-                $this->dompdf->loadHtml($data);
-
-                // (Optional) Setup the paper size and orientation
-                $this->dompdf->setPaper('A4', $orientation);
-
-                // Render the HTML as PDF
-                $this->dompdf->render();
-
-                // Output the generated PDF to Browser
-                $this->dompdf->stream();
-        }
-
-}
+//
+//defined('BASEPATH') OR exit('No direct script access allowed');
+//
+//require_once('assets/libraries/dompdf/autoload.inc.php');
+//
+//use Dompdf\Dompdf;
+//
+//class Pdf
+//{
+//
+//
+//        private $dompdf;
+//
+//        public function __construct()
+//        {
+//                $this->dompdf = new Dompdf();
+//        }
+//
+//        public function print_now($data, $orientation = 'portrait')
+//        {
+//                $this->dompdf->loadHtml($data);
+//
+//                // (Optional) Setup the paper size and orientation
+//                $this->dompdf->setPaper('A4', $orientation);
+//
+//                // Render the HTML as PDF
+//                $this->dompdf->render();
+//
+//                // Output the generated PDF to Browser
+//                $this->dompdf->stream();
+//        }
+//
+//}
