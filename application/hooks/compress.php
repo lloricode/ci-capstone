231c6cbecc46bcb61345a3d84237e8d8adcef9e4
lloricode <emorickfighter@gmail.com>
2017-04-18 20:16:09 +0800

    set enable_hook to TRUE,then update compress file. only tesing will test in unit_test

--- application/hooks/compress.php
+++ application/hooks/compress.php
@@ -2,26 +2,33 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$CI = & get_instance();
+$CI     = & get_instance();
 $buffer = $CI->output->get_output();
 
-$search = array(
-    '/\>[^\S ]+/s',
-    '/[^\S ]+\</s',
-    '/(\s)+/s',
-    '#(?://)?<!\[CDATA\[(.*?)(?://)?\]\]>#s',
-    '/\>(\s)+\</'
-);
-
-$replace = array(
-    '>',
-    '<',
-    '\\1',
-    "//&lt;![CDATA[\n" . '\1' . "\n//]]>",
-    '><'
-);
-
-$buffer = preg_replace($search, $replace, $buffer);
-
-$CI->output->set_output($buffer);
+$CI->output->set_output(_compress($buffer));
 $CI->output->_display();
+
+function _compress($buffer)
+{
+        if (ENVIRONMENT != 'production')
+        {
+                return $buffer;
+        }
+        $search = array(
+            '/\>[^\S ]+/s',
+            '/[^\S ]+\</s',
+            '/(\s)+/s',
+            '#(?://)?<!\[CDATA\[(.*?)(?://)?\]\]>#s',
+            '/\>(\s)+\</'
+        );
+
+        $replace = array(
+            '>',
+            '<',
+            '\\1',
+            "//&lt;![CDATA[\n" . '\1' . "\n//]]>",
+            '><'
+        );
+
+        return preg_replace($search, $replace, $buffer);
+}
