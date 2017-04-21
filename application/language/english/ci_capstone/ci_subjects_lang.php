d6cae34122df1159edf44c3a955d787241ee3928
iEdzar <lightningzeay@yahoo.com>
2017-04-21 23:38:51 +0800

    lang with external css for print

--- application/language/english/ci_capstone/ci_subjects_lang.php
+++ application/language/english/ci_capstone/ci_subjects_lang.php
@@ -9,22 +9,22 @@ defined('BASEPATH') or exit('Direct Script is not allowed');
  *         @shikai06
  */
 //Subject Form
-$lang['create_subject_heading']                   = 'Create Subject';
-$lang['create_subject_code_label']                = 'Subject Code:';
+$lang['create_subject_heading']                   = 'Create Course';
+$lang['create_subject_code_label']                = 'Course Code:';
 $lang['create_subject_description_label']         = 'Descriptive Title:';
-$lang['create_subject_unit_label']                = 'Subject Unit:';
+$lang['create_subject_unit_label']                = 'Course Unit:';
 $lang['create_subject_course_name_label']         = 'Program Name:';
-$lang['create_subject_submit_button_label']       = 'Create Subject';
-$lang['create_subject_succesfully_added_message'] = 'Subject Succesfully Added!!';
+$lang['create_subject_submit_button_label']       = 'Create Course';
+$lang['create_subject_succesfully_added_message'] = 'Course Succesfully Added!!';
 
 //Subject Table header
-$lang['index_subject_heading_th']     = 'Subjects';
-$lang['index_subject_code_th']        = 'Subject';
+$lang['index_subject_heading_th']     = 'Courses';
+$lang['index_subject_code_th']        = 'Course';
 $lang['index_subject_description_th'] = 'Descriptive Title';
 $lang['index_subject_unit_th']        = 'Unit';
 $lang['index_subject_course_name_th'] = 'Program';
 
-$lang['view_subject_label'] = 'View Subjects';
+$lang['view_subject_label'] = 'View Courses';
 
 
 
