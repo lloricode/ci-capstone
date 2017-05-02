4ed7b26d9a39a415d3c003eeae20702f3c12356c
shikai06 <pojinkee1@gmail.com>
2017-05-02 14:13:55 +0800

    form design

--- application/views/admin/create_subject_offer.php
+++ application/views/admin/create_subject_offer.php
@@ -17,87 +17,104 @@ if (isset($two_forms_conflict_message))
     <div class="row-fluid">
         <div class="span6">
 
-            <div class="widget-box">
-                <div class="widget-title"> <span class="icon"> <i class="icon-info-sign"></i> </span>
-                    <h5><?php echo lang('create_subject_offer_heading') ?></h5>
+            <div class="widget-box collapsible">
+                <div class="widget-title"> <a href="#collapseOne" data-toggle="collapse"> <span class="icon"><i class="icon-list"></i></span>
+                        <h5><?php echo lang('create_subject_offer_heading') ?></h5>
+                    </a> </div>
+
+                <?php
+                /**
+                 * @Contributor: Jinkee Po <pojinkee1@gmail.com>
+                 *         
+                 */
+                echo form_open(site_url('create-subject-offer'), array(
+                    'class'      => 'form-horizontal',
+                    'name'       => 'basic_validate',
+                    'id'         => 'basic_validate',
+                    'novalidate' => 'novalidate',
+                ));
+                /**
+                 * info
+                 */
+                echo input_bootstrap($user_id);
+                echo input_bootstrap($subject_id);
+                ?>
+                <div class="control-group">
+                    <div class="control-label">
+                        <h3>Schedule</h3>    
+                    </div>
                 </div>
+
+                <?php
+                /**
+                 * 1st
+                 *////////////////////////////////////////////////////////////////////////////////////////////
+                //subject_offer_start:
+                echo input_bootstrap($subject_offer_start);
+
+
+                //subject_offer_end:
+                echo input_bootstrap($subject_offer_end);
+                echo input_bootstrap($days1);
+                echo input_bootstrap($room_id);
+                echo input_bootstrap($leclab);
+                ?>
+                <div class="control-group">
+                    <div class="control-label">
+                        Check to exclude second form
+                    </div>
+                    <div class="controls">
+                        <?php echo form_checkbox('exclude', TRUE, set_checkbox('exclude', set_value('exclude'))); ?>
+                    </div>
+                </div>
+
+                <div class="widget-title"> <a href="#collapseTwo" data-toggle="collapse"> <span class="icon"><i class="icon-plus"></i></span>
+                        <h5>Another Class Schedule</h5>
+                    </a> </div>  
                 <div class="widget-content nopadding">
-                    <?php
-                    /**
-                     * @Contributor: Jinkee Po <pojinkee1@gmail.com>
-                     *         
-                     */
-                    echo form_open(site_url('create-subject-offer'), array(
-                        'class'      => 'form-horizontal',
-                        'name'       => 'basic_validate',
-                        'id'         => 'basic_validate',
-                        'novalidate' => 'novalidate',
-                    ));
-                    /**
-                     * info
-                     */
-                    echo input_bootstrap($user_id);
-                    echo input_bootstrap($subject_id);
-                    ?>
-                    <div class="control-group">
-                        <div class="control-label">
-                            <h3>Schedule</h3>    
+                    <div class="collapse" id="collapseTwo">
+                        <div class="widget-content"> 
+
+                            <?php
+                            /**
+                             * end
+                             *////////////////////////////////////////////////////////////////////////////////////////////
+                            //subject_offer_start:
+                            echo input_bootstrap($subject_offer_start2);
+
+
+                            //subject_offer_end:
+                            echo input_bootstrap($subject_offer_end2);
+                            echo input_bootstrap($days2);
+                            echo input_bootstrap($room_id2);
+                            echo input_bootstrap($leclab2);
+
+                            echo form_close();
+                            ?>
                         </div>
                     </div>
-
-                    <?php
-                    /**
-                     * 1st
-                     *////////////////////////////////////////////////////////////////////////////////////////////
-                    //subject_offer_start:
-                    echo input_bootstrap($subject_offer_start);
-
-
-                    //subject_offer_end:
-                    echo input_bootstrap($subject_offer_end);
-                    echo input_bootstrap($days1);
-                    echo input_bootstrap($room_id);
-                    echo input_bootstrap($leclab);
-                    ?>
-                    <div class="control-group">
-                        <div class="control-label">
-                            <h3>Schedule 2</h3>Check to exclude <?php echo form_checkbox('exclude', TRUE, set_checkbox('exclude', set_value('exclude'))); ?>
-                        </div>
-                    </div>
-
-                    <?php
-                    /**
-                     * end
-                     *////////////////////////////////////////////////////////////////////////////////////////////
-                    //subject_offer_start:
-                    echo input_bootstrap($subject_offer_start2);
-
-
-                    //subject_offer_end:
-                    echo input_bootstrap($subject_offer_end2);
-                    echo input_bootstrap($days2);
-                    echo input_bootstrap($room_id2);
-                    echo input_bootstrap($leclab2);
-
-                    echo ' <div class="form-actions">';
-
-
-                    echo form_reset('reset', 'Reset', array(
-                        'class' => 'btn btn-default'
-                    ));
-
-
-                    echo form_submit('submit', lang('create_subject_offer_submit_button_label'), array(
-                        'class' => 'btn btn-success'
-                    ));
-
-                    echo '</div>';
-                    echo form_close();
-                    ?>
-
                 </div>
+                <?php
+                echo ' <div class="form-actions">';
+
+
+                echo form_reset('reset', 'Reset', array(
+                    'class' => 'btn btn-default'
+                ));
+
+
+                echo form_submit('submit', lang('create_subject_offer_submit_button_label'), array(
+                    'class' => 'btn btn-success'
+                ));
+
+                echo '</div>';
+                echo form_close();
+                ?>
             </div>
         </div>
     </div>
+</div>
+</div>
+</div>
 </div>
 
